dba1d9e4df44e38ef74b684998311f075208169f
gyara658 <gyara658@gmail.com>
2021-12-18 16:01:05 +0900

    learn props of components

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
 
-function App() {
+
+
+const App = () => {
+  const profiles = [
+    { name: "Taro", age: 10},
+    { name: "Hanako", age: 5},
+  ]
   return (
-    <div><h1>Hello world</h1></div>
-  );
+    <div>
+    {profiles.map((profile, index) => {
+      return <User  name={profile.name} age={profile.age} key={index} />
+    })}
+    </div>
+  )
+}
+
+const User = (props) => {
+  return <div>Hi I am {props.name}, and {props.age} years old</div>
 }
 
 export default App;
